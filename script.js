0034cf36ba044a7cf8d6f3d05682df22383682f8
Sunny Bapla <98226491+s-bapla@users.noreply.github.com>
2024-05-13 07:32:47 -0700

    added img, transition

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ document.addEventListener("click", (e) => {
   const isMenuIcon = e.target === menuIcon;
   const navbar = document.querySelector(".navbar-container");
   const mainContent = document.querySelector(".main-content");
+  const img = document.querySelector("#world");
   if (isMenuIcon) {
     navbar.classList.toggle("active");
     mainContent.classList.toggle("active");
   }
-  if (e.target === mainContent) {
+  if (e.target === mainContent || e.target === img) {
     navbar.classList.remove("active");
     mainContent.classList.remove("active");
   }
@@ -18,4 +19,13 @@ let typed = new Typed("#typed-text", {
   typeSpeed: 50,
   backSpeed: 50,
   loop: true,
+});
+
+window.addEventListener("resize", (e) => {
+  const navbar = document.querySelector(".navbar-container");
+  const mainContent = document.querySelector(".main-content");
+  if (innerWidth > 768) {
+    navbar.classList.remove("active");
+    mainContent.classList.remove("active");
+  }
 });
